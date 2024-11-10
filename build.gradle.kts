b75eaa90eb8d7f5439e526b79dde2117e3e77d5f
root <root@DESKTOP-K12U5GE.localdomain>
2024-11-11 01:07:40 +0300

    Добавил папку src

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,9 @@ repositories {
 }
 
 dependencies {
+    api("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
